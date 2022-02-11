cdcf527ef7a79dd62ccd53f605fafe6110e00d1b
Jimmy Purnell <78944509+ledzep443@users.noreply.github.com>
2022-02-11 07:28:11 -0500

    Added database operations and caching to client

---
+++ Client/Static/APIEndpoints.cs
@@ -0,0 +1,18 @@
+﻿namespace Client.Static
+{
+    internal static class APIEndpoints
+    {
+#if DEBUG
+        internal const string ServerBaseUrl = "https://localhost:7245";
+#else
+        internal const string ServerBaseUrl = "";
+#endif
+
+        internal readonly static string s_categories = $"{ServerBaseUrl}/api/categories";
+        internal readonly static string s_categoriesWithPosts = $"{ServerBaseUrl}/api/categories/withposts";
+        internal readonly static string s_posts = $"{ServerBaseUrl}/api/posts";
+        internal readonly static string s_postsDTO = $"{ServerBaseUrl}/api/posts/dto";
+        internal readonly static string s_imageUpload = $"{ServerBaseUrl}/api/imageupload";
+        internal readonly static string s_signIn = $"{ServerBaseUrl}/api/signin";
+    }
+}
