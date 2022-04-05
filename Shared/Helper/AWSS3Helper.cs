06035841dce0a98341b0246f886b97b5a1f792b1
Jimmy Purnell <78944509+ledzep443@users.noreply.github.com>
2022-04-05 00:37:31 -0400

    Fixed image resize issue, and started AWSS3 helper

---
+++ Shared/Helper/AWSS3Helper.cs
@@ -0,0 +1,26 @@
+﻿using Shared.Helper.IHelper;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Helper
+{
+    public class AWSS3Helper : IAWSS3Helper
+    {
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1;
+        private static IAmazonS3 client;
+
+        public AWSS3Helper()
+        {
+            client = new AmazonS3Client(bucketRegion);
+           
+        }
+
+        
+    }
+}
